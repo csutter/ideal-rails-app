0e0235564961461754b70d842e35dcdf787b1d6c
Christian Sutter <git@doublesignal.com>
2023-02-27 15:14:49 +0000

    Add initial post create script for devcontainer

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eoux pipefail
+
+echo 'Ensure entire workspace (including mounted Docker volumes) is owned by non-privileged user'
+sudo chown -R $(id -u):$(id -g) .
+
+echo 'Install Ruby dependencies'
+bundle install
+
+echo 'Ensure `tmp` folder has `.keep` files'
+# (these may be present on the host, but the container uses a volume for the `tmp` directory,
+# leading Git to believe the `.keep` file has gone missing)
+mkdir -p tmp/{pids,storage}
+touch {tmp,tmp/pids,tmp/storage}/.keep
