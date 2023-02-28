c44a8a1316d506ec96bd41eb65066a9e49317aba
Christian Sutter <git@doublesignal.com>
2023-02-28 15:03:19 +0000

    Install JS dependencies during devcontainer post create

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -7,6 +7,9 @@ sudo chown -R $(id -u):$(id -g) .
 echo 'Install Ruby dependencies'
 bundle install
 
+echo 'Install Javascript dependencies'
+yarn install
+
 echo 'Ensure `tmp` folder has `.keep` files'
 # (these may be present on the host, but the container uses a volume for the `tmp` directory,
 # leading Git to believe the `.keep` file has gone missing)
